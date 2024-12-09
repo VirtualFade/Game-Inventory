85e0db2d85f2d2182d7dd61c0d0f95820f616792
Ace13505 <141468978+Ace13505@users.noreply.github.com>
2024-12-08 22:48:17 -0800

    Update PhysicalGame.cpp

--- Game Inventory/PhysicalGame.cpp
+++ Game Inventory/PhysicalGame.cpp
@@ -1,1 +1,32 @@
 #include "PhysicalGame.h"
+
+PhysicalGame::PhysicalGame() 
+    : Game(), isCollectorEdition(false), discSize(0.0f) {}
+
+PhysicalGame::PhysicalGame(const string& title, const string& genre, int year, const Platform& platform, bool isCollectorEdition, float discSize)
+    : Game(title, genre, year, platform), isCollectorEdition(isCollectorEdition), discSize(discSize) {}
+
+void PhysicalGame::setIsCollectorEdition(bool isCollectorEdition) {
+    this->isCollectorEdition = isCollectorEdition;
+}
+
+void PhysicalGame::setDiscSize(float discSize) {
+    this->discSize = discSize;
+}
+
+bool PhysicalGame::getIsCollectorEdition() const {
+    return isCollectorEdition;
+}
+
+float PhysicalGame::getDiscSize() const {
+    return discSize;
+}
+
+void PhysicalGame::displayInfo() const {
+    // Call the base class method to print common info
+    Game::printInfo();
+
+    // Add specific information for PhysicalGame
+    cout << "Collector's Edition: " << (isCollectorEdition ? "Yes" : "No") << endl;
+    cout << "Disc Size: " << discSize << " GB" << endl;
+}
