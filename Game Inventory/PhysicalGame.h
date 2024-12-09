1e2e26ef350fb969c78cf255ea562d02092d21ef
Ace13505 <141468978+Ace13505@users.noreply.github.com>
2024-12-08 22:47:47 -0800

    Update PhysicalGame.h

--- Game Inventory/PhysicalGame.h
+++ Game Inventory/PhysicalGame.h
@@ -1,5 +1,32 @@
 #pragma once
-class PhysicalGame
-{
+#ifndef PHYSICALGAME_H
+#define PHYSICALGAME_H
+
+#include "Game.h"
+#include <iostream>
+using namespace std;
+
+class PhysicalGame : public Game {
+private:
+    bool isCollectorEdition;  // Whether it's a collector's edition
+    float discSize;           // Size of the disc in GB
+
+public:
+    PhysicalGame();
+    PhysicalGame(const string& title, const string& genre, int year, const Platform& platform, bool isCollectorEdition, float discSize);
+
+    // Setters
+    void setIsCollectorEdition(bool isCollectorEdition);
+    void setDiscSize(float discSize);
+
+    // Getters
+    bool getIsCollectorEdition() const;
+    float getDiscSize() const;
+
+    // Override displayInfo to include new attributes
+    void displayInfo() const override;
 };
+
+#endif
+
 
