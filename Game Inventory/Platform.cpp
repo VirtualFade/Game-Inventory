cc5ee47258d049baa611a33b98d398a33773d988
Ace13505 <141468978+Ace13505@users.noreply.github.com>
2024-12-08 23:07:38 -0800

    Update Platform.cpp

--- Game Inventory/Platform.cpp
+++ Game Inventory/Platform.cpp
@@ -1,5 +1,14 @@
 #include "Platform.h"
 
+// Predefined platforms
+Platform ps5("PS5", "Sony", 2020);
+Platform xbox("Xbox Series X", "Microsoft", 2020);
+Platform switchConsole("Nintendo Switch", "Nintendo", 2017);
+
+// Array or list of predefined platforms for selection
+Platform platforms[] = {ps5, xbox, switchConsole};
+const int NUM_PLATFORMS = 3; // Number of platforms available
+
 Platform::Platform() : name("Unknown"), manufacturer("Unknown"), releaseYear(0) {}
 
 Platform::Platform(const string& name, const string& manufacturer, int releaseYear) : name(name), manufacturer(manufacturer), releaseYear(releaseYear) {}
